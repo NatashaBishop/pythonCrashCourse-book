1aa77d37d851256940e53f3f2b15d2cbf18344e1
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-09-19 17:41:04 +0100

    Create variablesInString.py

---
+++ variablesInString.py
@@ -0,0 +1,6 @@
+firstName = "natasha"
+lastName = "bishop"
+# by puting f infront of the string we allow variables 2 b imbeded
+# we can also mix variables with strings
+print(f"{firstName} OK with me {lastName}".title())
+# title() converts first letters of words to upperclass
