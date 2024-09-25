e153543d477a2ede8cc615c050bdae8e9d7c0ac4
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-09-25 15:54:24 +0100

    Update f-string_withListItem.py

--- f-string_withListItem.py
+++ f-string_withListItem.py
@@ -1,3 +1,7 @@
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
-message = f"My first bicycle was a {bicycles[-2].title()}." # [-2] - this index takes 2nd element from the end of the list
+message = f"My first bicycle was a {bicycles[-2].title()}." 
+# [-2] - this index takes 2nd element from the end of the list
 print(message)
+
+# Output
+# My first bicycle was a Redline.
